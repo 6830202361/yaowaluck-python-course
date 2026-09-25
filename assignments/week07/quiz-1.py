9206dae0c08b534f39e59a3811955c6fbddc03a9
Yaowaluck jitprajon <yaowaluck.ji@ku.th>
2026-09-25 19:45:23 +0700

    OOP1

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -22,3 +22,27 @@ class Rectangle:
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+
+"""
+ขอให้เขียนคลาส Circle ที่ทำงานคล้ายคลึงกับคลาส Rectagle พร้อมตัวอย่างการใช้งาน
+"""
+import math
+
+class Circle:
+    # ปรับให้สอดคล้องกับความเป็นวงกลม
+    def _init__(self, radius):
+        self.radius = radius
+
+    # Method to get the area ปรับสูตรรพท. วงกลม
+    def get_area(self):
+        return math.pi * self radius ** 2
+
+    # Method to get the perimeter ปรับสูตรเส้นรอบรูปวงกลม
+    def get_perimeter(self):
+        return 2 * (math.pi * self radius)
+
+
+myCircle = Circle(10)
+print(myCircle.get_area())
+print(myCircle.get_perimeter())
